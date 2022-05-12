a979a4d18aa8910b5ebf0805f0359a3d0942a965
Juan Saucedo <julusaucedoga@ittepic.edu.mx>
2022-05-12 16:39:46 -0600

    SegundaParte

--- app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis/ui/BuscarMascota/BuscarMascotaFragment.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis/ui/BuscarMascota/BuscarMascotaFragment.kt
@@ -6,9 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.R
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.databinding.ActualizarMascotaFragmentBinding
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.databinding.BuscarMascotaFragmentBinding
+import java.lang.NullPointerException
 
 class BuscarMascotaFragment : Fragment() {
 
@@ -25,7 +31,83 @@ class BuscarMascotaFragment : Fragment() {
     ): View? {
         binding= BuscarMascotaFragmentBinding.inflate(inflater,container,false)
         val root: View=binding.root
+
+        var idGlobal=""
+        var listaID = ArrayList<String>()
+        val baseRemota2= FirebaseFirestore.getInstance()
+
+        FirebaseFirestore.getInstance().collection("mascota")
+            .addSnapshotListener { query, error ->
+                if (error != null) {
+                    //ERROR
+                    AlertDialog.Builder(requireContext())
+                        .setMessage(error.message)
+                        .show()
+                    return@addSnapshotListener
+                }
+
+                listaID.clear()
+                val llenar = ArrayList<String>()
+                for (documento in query!!) {
+                    var cadena =
+                        "${documento.getString("nombre")} ${documento.getString("raza")}"
+                    llenar.add(cadena)
+                    listaID.add(documento.id)
+                }
+                try {
+                    binding.lista.adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, llenar)
+                    binding.lista.setOnItemClickListener { adapterView, view, pos, l ->
+                        val idEl = listaID.get(pos)
+                        idGlobal = idEl
+                    }
+                } catch (err: NullPointerException) {
+                }
+            }
+
+
+        binding.btnBuscarMascota.setOnClickListener{
+            var select =baseRemota2.collection("mascota")
+
+            if(binding.etNombre.text.toString()==""){
+                if(binding.etPropietario.text.toString()==""){
+                    if (binding.etRaza.text.toString()==""){
+                        Toast.makeText(requireContext(),"Ingrese un parametro de busqueda", Toast.LENGTH_LONG)
+                            .show()
+                    }else{
+                        var select=baseRemota2.collection("mascota").whereEqualTo("raza",binding.etRaza.text.toString())
+                        mostrarLista(select)
+                    }
+                }else{
+                    var select=baseRemota2.collection("mascota").whereEqualTo("curp",binding.etPropietario.text.toString())
+                    mostrarLista(select)
+                }
+            }else{
+                var select=baseRemota2.collection("mascota").whereEqualTo("nombre",binding.etNombre.text.toString())
+                mostrarLista(select)
+            }
+        }
+
+
         return root
+    }
+    fun mostrarLista(qur : Query) {
+        qur.addSnapshotListener { query, error ->
+            if (error != null) {
+                //ERROR
+                AlertDialog.Builder(requireContext())
+                    .setMessage(error.message)
+                    .show()
+                return@addSnapshotListener
+            }
+            val llenar = ArrayList<String>()
+            for (documento in query!!) {
+                var cadena =
+                    "${documento.getString("nombre")} ${documento.getString("raza")} "
+                llenar.add(cadena)
+            }
+            binding.lista.adapter =
+                ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, llenar)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis/ui/InsertarMascota/InsertarMascotaFragment.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis/ui/InsertarMascota/InsertarMascotaFragment.kt
@@ -6,9 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.R
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.databinding.ActualizarMascotaFragmentBinding
 import mx.tecnm.tepic.ladm_u3_practica2_firebaseveterinaria_saucedogarciajuanluis.databinding.InsertarMascotaFragmentBinding
+import java.lang.NullPointerException
 
 class InsertarMascotaFragment : Fragment() {
 
@@ -25,6 +31,87 @@ class InsertarMascotaFragment : Fragment() {
     ): View? {
         binding= InsertarMascotaFragmentBinding.inflate(inflater,container,false)
         val root: View=binding.root
+
+        var listaID = ArrayList<String>()
+        val baseRemota= FirebaseFirestore.getInstance()
+        var idGlobal=""
+        var curpGlobal=""
+
+
+        FirebaseFirestore.getInstance().collection("propietario")
+            .addSnapshotListener { query, error ->
+                if (error != null) {
+                    //ERROR
+                    AlertDialog.Builder(requireContext())
+                        .setMessage(error.message)
+                        .show()
+                    return@addSnapshotListener
+                }
+
+                listaID.clear()
+                val llenar = ArrayList<String>()
+                for (documento in query!!) {
+                    var cadena =
+                        "${documento.getString("curp")} ${documento.getString("nombre")}  ${
+                            documento.getLong("edad")
+                        } años"
+                    llenar.add(cadena)
+                    listaID.add(documento.id)
+                }
+                try {
+                    binding.lista.adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, llenar)
+                    binding.lista.setOnItemClickListener { adapterView, view, pos, l ->
+                        val idEl = listaID.get(pos)
+                        idGlobal = idEl
+                    }
+                } catch (err: NullPointerException) {
+                }
+            }
+        binding.btnBuscarProp.setOnClickListener {
+            baseRemota.collection("propietario")
+                .whereEqualTo("nombre", binding.etNombreProp.text.toString()).get()
+                .addOnSuccessListener {
+                    lateinit var curp: String
+                    for (documento in it) {
+                        curpGlobal = documento.getString("curp").toString()
+                        binding.getCurp.setText(documento.getString("curp"))
+                    }
+                }
+                    binding.btnAgregarMascota.setOnClickListener {
+                        val baseRemota2 = FirebaseFirestore.getInstance()
+                        baseRemota2.collection("mascota").get()
+                            .addOnSuccessListener {
+                                for (documento in it) {
+                                    val datos = hashMapOf(
+                                        "nombre" to binding.etNombre.text.toString(),
+                                        "raza" to binding.etRaza.text.toString(),
+                                        "curp" to curpGlobal
+                                    )
+                                    baseRemota2.collection("mascota").add(datos)
+                                        .addOnSuccessListener{
+                                            // SÍ SE PUDO
+                                            Toast.makeText(requireContext(), "Exito! Sí se pudo", Toast.LENGTH_LONG).show()
+                                            binding.etNombreProp.setText("")
+                                            binding.etNombre.setText("")
+                                            binding.etRaza.setText("")
+                                            binding.getCurp.setText("Seleccione un propietario")
+                                        }
+                                        .addOnFailureListener {
+                                            // NO SE PUDO
+                                            AlertDialog.Builder(requireContext())
+                                                .setMessage(it.message)
+                                                .show()
+
+                                        }
+                                }
+                            }
+                    }
+
+        }
+
+
+
+
         return root
     }
 
